ed7868a13fe641c1ff4688017f8462aa829f0b9e
machuret <gmachuret@gmail.com>
2026-05-02 22:04:12 +1000

    Trigger new deployment

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
 import { cruiseLines, accidentTypes, destinations } from "@/data/site-data";
+
+// Trigger redeploy
 import { getLastUpdated } from "@/lib/seo";
 import { JurisdictionAlertBlock } from "@/components/blocks/JurisdictionAlertBlock";
 import { LeadFormBlock } from "@/components/blocks/LeadFormBlock";
